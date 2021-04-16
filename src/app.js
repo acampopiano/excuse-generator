b7c632377cb44c6bd275171399a8262e5af39d12
Andres Campopiano <acampopiano@gmail.com>
2021-04-16 13:18:23 +0000

    subo excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let valueToUseWho = who[Math.floor(Math.random() * who.length)];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let valueToUseAction = action[Math.floor(Math.random() * action.length)];
+  let what = ["my homework", "the keys", "the car"];
+  let valueToUseWhat = what[Math.floor(Math.random() * what.length)];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let valueToUseWhen = when[Math.floor(Math.random() * when.length)];
+  let excuses = [
+    valueToUseWho,
+    valueToUseAction,
+    valueToUseWhat,
+    valueToUseWhen
+  ];
+  var excuse = "";
+  excuses.forEach(element => {
+    excuse += element + " ";
+  });
+  document.getElementById("excuse").innerHTML = excuse;
 };
